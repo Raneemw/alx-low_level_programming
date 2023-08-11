48525c5e5992f02e4d7186c2c2813bb774a6dcca
Raneemw <Raneemw092@gmail.com>
2023-08-11 12:52:46 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Always 0 (Success)
- */
+ * Description:A C program that prints with put function
+ * Return:Always 0 (success)
+*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}       
+}
